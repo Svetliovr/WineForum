ead199004b7d5b048a5515c4f3a2942b66940a73
SVRusev <svvasilev978@gmail.com>
2020-06-05 11:38:54 +0300

    one

---
+++ WineForum.Data/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace WineForum.Data.Models
+
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string UserDescription { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public int Rating { get; set; }
+        public DateTime MemberSince { get; set; }
+    }
+}
---
+++ WineForum/Controllers/ForumController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WineForum.Data.Models;
+using WineForum.Models.Forum;
+
+namespace WineForum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController (IForum forumService)
+        {
+            _forumService = forumService;
+        }
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+               .Select(forum => new ForumListingModel
+               {
+                   Id = forum.Id,
+                   Name = forum.Title,
+                   Description = forum.Description,
+               });
+
+            return View();
+        }
+    }
+}
