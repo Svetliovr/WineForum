c67b3206436820e9a4c2d7dd59b6af22b0dfcac1
SVRusev <svvasilev978@gmail.com>
2020-06-06 15:42:54 +0300

    appuser and upload interfaces made ,services updated

---
+++ WineForum.Data/IApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WineForum.Data.Models;
+
+namespace WineForum.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+        Task SetProfileImage(string id, Uri uri);
+        Task UpdateUserRating(string id, Type type);
+    }
+}
---
+++ WineForum/Controllers/ProfileController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WineForum.Data;
+using WineForum.Data.Models;
+using WineForum.Models.ApplicationUser;
+
+namespace WineForum.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager,
+            IApplicationUser userService,
+            IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        public IActionResult Detail(string id)
+        {
+            var user = _userService.GetById(id);
+            var userRoles = _userManager.GetRolesAsync(user).Result;
+
+            var model = new ProfileModel()
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                UserRating = user.Rating.ToString(),
+                ProfileImageUrl = user.ProfileImageUrl,
+                MemberSince = user.MemberSince,
+                IsAdmin = userRoles.Contains("Admin")
+            };
+            return View(model);
+        }
+    }
+}
